cb353724666081ef1c3252ee6ef7a0ed6789c01f
unknown <pallavisahay01@gmail.com>
2020-09-27 12:56:54 +0530

    my first page.

---
+++ restaurantdesign/js/script.js
@@ -0,0 +1,14 @@
+// Remove the class 'active' from home and switch to Menu button
+var switchMenuToActive = function () {
+  // Remove 'active' from home button
+  var classes = document.querySelector("#navHomeButton").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector("#navHomeButton").className = classes;
+
+  // Add 'active' to menu button if not already there
+  classes = document.querySelector("#navMenuButton").className;
+  if (classes.indexOf("active") == -1) {
+    classes += " active";
+    document.querySelector("#navMenuButton").className = classes;
+  }
+};
